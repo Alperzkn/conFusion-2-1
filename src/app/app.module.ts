63e5e7e6dc34cab4f048c05a6b1f240c9f786243
Alper Özkan <mrsecret@windowslive.com>
2017-12-14 16:09:54 +0300

    First Week Assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from '@angular/material';
+import {MdToolbarModule, MdButtonModule, MdCheckboxModule,MdListModule,MdGridListModule} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MaterialModule,
+    MdToolbarModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdListModule,
+    MdGridListModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
